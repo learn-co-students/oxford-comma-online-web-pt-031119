efa12610bf13dd8f1f9a4a9b5a44c298226a5538
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-03-20 23:20:43 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.join
+  end
+  if array.length == 2
+    return array.join(" and ")
+  end
+  if array.length >= 3
+    word = array.pop
+    array.insert(array.length, 'and ')
+    str = array.join(', ') + word
+    return str
+  end
 end
